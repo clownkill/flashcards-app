d54368039c2ff72ea3ea260a1df4918e03110347
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-07-19 22:35:35 +0300

    add boxes to navigation

--- cards/urls.py
+++ cards/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.CardListView.as_view(), name='card-list'),
     path('new/', views.CardCreateView.as_view(), name='card-create'),
-    path('edit/<int:pk>', views.CardUpdateView.as_view(), name='card-update'),
+    path('edit/<int:pk>/', views.CardUpdateView.as_view(), name='card-update'),
+    path('box/<int:box_num>/', views.BoxView.as_view(), name='box'),
 ]
