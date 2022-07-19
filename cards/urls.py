e81e7659e407265b4aa9c0f85df9b3d30e43f843
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-07-19 21:24:41 +0300

    add editing card

--- cards/urls.py
+++ cards/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.CardListView.as_view(), name='card-list'),
     path('new/', views.CardCreateView.as_view(), name='card-create'),
+    path('edit/<int:pk>', views.CardUpdateView.as_view(), name='card-update'),
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,6 +1,9 @@
 from django.urls import reverse_lazy
-from django.views.generic import (ListView,
-                                  CreateView)
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+)
 
 from .models import Card
 
@@ -14,3 +17,7 @@ class CardCreateView(CreateView):
     model = Card
     fields = ['question', 'answer', 'box']
     success_url = reverse_lazy('card-create')
+
+
+class CardUpdateView(CardCreateView, UpdateView):
+    success_url = reverse_lazy('card-list')
