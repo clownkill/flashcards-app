0b40ea63c7ba91025bb7b462cb78a3a65c1c8d9c
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-07-19 20:52:30 +0300

    add card_list.html

--- cards/models.py
+++ cards/models.py
@@ -1,2 +1,25 @@
 from django.db import models
 
+NUM_BOXES = 5
+BOXES = range(1, NUM_BOXES + 1)
+
+
+class Card(models.Model):
+    question = models.CharField(
+        'Вопрос',
+        max_length=100
+    )
+    answer = models.CharField(
+        'Ответ',
+        max_length=100
+    )
+    box = models.IntegerField(
+        'Бокс вопроса',
+        choices=zip(BOXES, BOXES),
+        default=BOXES[0],
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
+
